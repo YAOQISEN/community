453312cf85e3448bc8f8f2cdb509db64fdf9d0d5
YAOQISEN <605217467@qq.com>
2019-10-25 03:51:29 +0800

    save user infomation in mysql with mybatis

--- src/test/java/com/guison/community/community/CommunityApplicationTests.java
+++ src/test/java/com/guison/community/community/CommunityApplicationTests.java
@@ -1,10 +1,19 @@
 package com.guison.community.community;
 
+import com.guison.community.community.mapper.UserMapper;
+import com.guison.community.community.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
 
 @SpringBootTest
 class CommunityApplicationTests {
+
+	@Test
+	void getUser(){
+	}
 
 	@Test
 	void contextLoads() {
